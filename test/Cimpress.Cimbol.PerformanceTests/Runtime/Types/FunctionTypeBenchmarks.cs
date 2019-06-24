33a75205123c57ff0613472f7070b50e93a71dee
asalani <asalani@cimpress.com>
2019-06-24 12:27:46 -0400

    Create benchmark tests
    The benchmarks are stored as artifacts but are not yet uploaded
    anywhere useful.

---
+++ test/Cimpress.Cimbol.PerformanceTests/Runtime/Types/FunctionTypeBenchmarks.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using Cimpress.Cimbol.Runtime.Types;
+
+namespace Cimpress.Cimbol.PerformanceTests.Runtime.Types
+{
+    public class FunctionTypeBenchmarks
+    {
+        private FunctionValue _arithmeticFunction;
+
+        private FunctionValue _identityFunction;
+
+        private NumberValue _testNumber1;
+
+        private NumberValue _testNumber2;
+
+        private StringValue _testString1;
+
+        private StringValue _testString2;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            _arithmeticFunction = new FunctionValue((Func<NumberValue, NumberValue, NumberValue>)ArithmeticFunction);
+
+            _identityFunction = new FunctionValue((Func<NumberValue, NumberValue>)IdentityFunction);
+
+            _testNumber1 = new NumberValue(3);
+
+            _testNumber2 = new NumberValue(5);
+
+            _testString1 = new StringValue("3");
+
+            _testString2 = new StringValue("5");
+        }
+
+        [Benchmark]
+        public ILocalValue Benchmark_IdentityFunctionInvocation()
+        {
+            return _identityFunction.Invoke(_testNumber1);
+        }
+
+        [Benchmark]
+        public ILocalValue Benchmark_ArithmeticFunctionInvocation()
+        {
+            return _arithmeticFunction.Invoke(_testNumber1, _testNumber2);
+        }
+
+        private static NumberValue ArithmeticFunction(NumberValue number1, NumberValue number2)
+        {
+            return new NumberValue(number1.Value + number2.Value);
+        }
+
+        private static NumberValue IdentityFunction(NumberValue number)
+        {
+            return number;
+        }
+    }
+}
---
+++ test/Cimpress.Cimbol.PerformanceTests/Runtime/Types/ObjectTypeBenchmarks.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using Cimpress.Cimbol.Runtime.Types;
+
+namespace Cimpress.Cimbol.PerformanceTests.Runtime.Types
+{
+    public class ObjectTypeBenchmarks
+    {
+        private ObjectValue _object;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            var objectContents = new Dictionary<string, ILocalValue>(StringComparer.OrdinalIgnoreCase)
+            {
+                { "x", new NumberValue(3) },
+                { "y", new StringValue("test") },
+                { "z", new BooleanValue(true) },
+            };
+
+            _object = new ObjectValue(objectContents);
+        }
+
+        [Benchmark]
+        public ILocalValue Benchmark_ObjectAccess()
+        {
+            return _object.Access("y");
+        }
+    }
+}
